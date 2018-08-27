a3372b3c7391c496e59a6fe4f51d880328cfc7de
Coleman Word <40253900+ops2go@users.noreply.github.com>
2018-08-27 15:22:46 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# configure the aws client to use your new IAM user
-aws configure           # Use your new access and secret key here                           \
-&& export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)                           \
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)                              \
 && export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)                    \
-&& export NAME=kops.gotask.com                                                                \
-&& export KOPS_STATE_STORE=s3://kops-gotask-io-state-store                                    \
-&& kops create cluster                                                                        \
-    --zones us-east-1a                                                                        \
-    ${NAME}
+&& export NAME=kops.gotask.io                                                                \
+&& export KOPS_STATE_STORE=s3://kops-gotask-io-state-store                                    
+
