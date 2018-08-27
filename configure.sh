21389de327408a5943d613d7e75339a944b49bbd
Coleman Word <40253900+ops2go@users.noreply.github.com>
2018-08-27 15:18:45 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,4 +4,7 @@ aws configure           # Use your new access and secret key here               
 && export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)                           \
 && export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)                    \
 && export NAME=kops.gotask.com                                                                \
-&& export KOPS_STATE_STORE=s3://kops-gotask-io-state-store
+&& export KOPS_STATE_STORE=s3://kops-gotask-io-state-store                                    \
+&& kops create cluster                                                                        \
+    --zones us-east-1a                                                                        \
+    ${NAME}
