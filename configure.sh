dde9aee844b9207fa0cf34f90615fbac123f4c1d
Coleman Word <40253900+ops2go@users.noreply.github.com>
2018-08-27 15:09:36 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # configure the aws client to use your new IAM user
-aws configure           # Use your new access and secret key here
-aws iam list-users      # you should see a list of all your IAM users here
-
-# Because "aws configure" doesn't export these vars for kops to use, we export them now
-export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+aws configure           # Use your new access and secret key here                           \
+&& aws iam list-users      # you should see a list of all your IAM users here               \
+&& export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)                           \
+&& export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
